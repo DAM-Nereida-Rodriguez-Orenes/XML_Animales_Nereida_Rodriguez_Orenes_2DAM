11fa9bd02117689cf4b9d8e1e1eda2e3cb4f484d
NereidaRO <nereidarodriguezorenes@gmail.com>
2026-02-04 20:50:32 +0100

    added generate XML feature

--- Animales-XML-Nereida-Rodriguez-Orenes-2DAM/src/animals/VentanaAnimales.java
+++ Animales-XML-Nereida-Rodriguez-Orenes-2DAM/src/animals/VentanaAnimales.java
@@ -49,6 +49,11 @@ public class VentanaAnimales extends javax.swing.JFrame {
         });
 
         btnGenerar.setText("Generar XML");
+        btnGenerar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGenerarActionPerformed(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Animales a XML");
@@ -129,6 +134,35 @@ public class VentanaAnimales extends javax.swing.JFrame {
             javax.swing.JOptionPane.showMessageDialog(this, "Introduce un nombre."); 
         }
     }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void btnGenerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGenerarActionPerformed
+        if (animales.size() != 5) { 
+            //si no hay 5 animales no generamos el XML
+            javax.swing.JOptionPane.showMessageDialog(this, "Debes introducir exactamente cinco animales."); 
+            return; 
+        }
+        
+        try { 
+            javax.xml.parsers.DocumentBuilderFactory factory = javax.xml.parsers.DocumentBuilderFactory.newInstance(); 
+            javax.xml.parsers.DocumentBuilder builder = factory.newDocumentBuilder(); 
+            org.w3c.dom.Document doc = builder.newDocument();
+            org.w3c.dom.Element raiz = doc.createElement("animales"); 
+            doc.appendChild(raiz);
+            for (String a : animales) {
+                org.w3c.dom.Element nodoAnimal = doc.createElement("animal");
+                nodoAnimal.setTextContent(a); raiz.appendChild(nodoAnimal);
+            } 
+            javax.xml.transform.TransformerFactory tf = javax.xml.transform.TransformerFactory.newInstance();
+            javax.xml.transform.Transformer transformer = tf.newTransformer();
+            javax.xml.transform.dom.DOMSource origen = new javax.xml.transform.dom.DOMSource(doc);
+            java.io.File archivo = new java.io.File("animales.xml");
+            javax.xml.transform.stream.StreamResult destino = new javax.xml.transform.stream.StreamResult(archivo);
+            transformer.transform(origen, destino);
+            javax.swing.JOptionPane.showMessageDialog(this, "XML generado correctamente.");
+        }catch (Exception ex) { 
+            ex.printStackTrace(); 
+        }
+    }//GEN-LAST:event_btnGenerarActionPerformed
 
     /**
      * @param args the command line arguments
